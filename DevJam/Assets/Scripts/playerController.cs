ecb0d57b45617ea8d98b5269177cba772debac12
emirbaykal <83282141+emirbaykal@users.noreply.github.com>
2022-05-13 21:30:56 +0300

    push5Emir

--- DevJam/Assets/Scripts/playerController.cs
+++ DevJam/Assets/Scripts/playerController.cs
@@ -6,13 +6,17 @@ public class playerController : MonoBehaviour
 {
     [SerializeField] float Hiz;
     Rigidbody2D rb;
-    
+
     void Start () 
     {
         rb = GetComponent<Rigidbody2D>();
     }
     void Update () 
     {
+        if (Input.GetKey(KeyCode.Space))
+        {
+            rb.AddForce(new Vector2(0,7));
+        }
         float yatay = Input.GetAxis("Horizontal");
         Vector3 hareket = new Vector3(yatay*Hiz, 0.0f); 
         rb.AddForce(hareket);
